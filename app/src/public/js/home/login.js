601ef9888850d09c637a2634e0b290f84df72de3
codename-123 <wltkd5139@naver.com>
2025-03-24 16:49:10 +0900

    객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,1 +1,17 @@
-console.log('hello')
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('btn')
+    const id = document.getElementById('id')
+    const password = document.getElementById('password')
+    btn.addEventListener('click', () => {
+        login()
+        
+    })
+
+    function login() {
+        const req = {
+            id : id.value,
+            password : password.value
+        }
+        console.log(req)
+    }
+})
